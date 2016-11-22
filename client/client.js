420b4bddd27a48bc1d19d144087c0afdc8bb5cec
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-11-22 14:54:06 -0500

    part 3

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
+import React from 'react'
+// just import render method from react-dom
+// curly brace syntax lets you chain methods
+import { render } from 'react-dom'
+import App from '../components/App'
 
+render(
+  // first element: define the encompassing component that we want to mount
+  <App />,
+  // second element: DOM element we want to mount it to
+  document.getElementById('app')
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,10 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+  render() {
+    return <div>This is definitely a React app now!</div>
+  }
+}
+
+// allows you to import it later
+export default App
